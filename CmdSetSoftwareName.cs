36f4ad60f6da3671bd200eb4187bd795d257406f
UnknownShadow200 <unknownshadow200@gmail.com>
2018-02-21 13:31:29 +1100

    Create CmdSetSoftwareName.cs

---
+++ CmdSetSoftwareName.cs
@@ -0,0 +1,20 @@
+
+using System;
+using MCGalaxy;
+
+namespace MyCommandThingy {
+    public sealed class CmdSetSoftwareName : Command {
+        public override string name { get { return "SetSoftwareName"; } }
+        public override string type { get { return "other"; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Nobody; } }
+
+        public override void Use(Player p, string message) {
+        	Server.SoftwareName = message;
+        }
+
+        public override void Help(Player p) {
+        	Player.Message(p, "%T/SetSoftwareName [software name]");
+        	Player.Message(p, "%HSets software name to the given name.");
+        }
+    }
+}
