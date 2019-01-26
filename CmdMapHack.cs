fb0da3285480cdfdf09c52fd8d0ab6bb72604b35
UnknownShadow200 <unknownshadow200@gmail.com>
2019-01-26 18:16:15 +1100

    cleanup MapHack, fix 'only operator+ can bypass hacks' always showing

--- CmdMapHack.cs
+++ CmdMapHack.cs
@@ -6,9 +6,9 @@ namespace MCGalaxy {
 	public class CmdMaphack : Command2 {
 		public override string name { get { return "MapHack"; } }
 		public override string type { get { return "other"; } }
-		 public override CommandPerm[] ExtraPerms {
-            get { return new[] { new CommandPerm(LevelPermission.Operator, "can bypass hack restrictions on all maps") }; }
-        }
+		public override CommandPerm[] ExtraPerms {
+			get { return new[] { new CommandPerm(LevelPermission.Operator, "can bypass hack restrictions on all maps") }; }
+		}
 		
 		static bool hooked;
 		const string ext_allowed_key = "__MAPHACK_ALLOWED";
@@ -20,21 +20,21 @@ namespace MCGalaxy {
 				hooked = true;
 			}
 
-			if (CheckExtraPerm(p, data, 1) || LevelInfo.IsRealmOwner(p.name, p.level.MapName)) {
-				p.Extras.PutBoolean(ext_allowed_key, true);
+			if (LevelInfo.IsRealmOwner(p.name, p.level.MapName) || CheckExtraPerm(p, data, 1)) {
+				p.Extras[ext_allowed_key] = true;
 				p.SendMapMotd();
-				Player.Message(p, "&aYou are now bypassing hacks restrictions on this map");
+				p.Message("&aYou are now bypassing hacks restrictions on this map");
 			} else {
-				Player.SendMessage(p, "&cYou can only bypass hacks on your own realms.");
+				p.Message("&cYou can only bypass hacks on your own realms.");
 			}
 		}
 		
 		void HandleOnSentMap(Player p, Level prevLevel, Level level) {
 			if (!p.Extras.GetBoolean(ext_allowed_key)) return;
 			// disable /maphack when you reload or change maps
-			p.Extras.PutBoolean(ext_allowed_key, false);
+			p.Extras[ext_allowed_key] = false;
 			p.SendMapMotd();
-			Player.Message(p, "%HHacks bypassing reset, use %T/MapHack %Hto turn on again");
+			p.Message("%HHacks bypassing reset, use %T/MapHack %Hto turn on again");
 		}
 		
 		void HandleGettingMotd(Player p, ref string motd) {
@@ -43,9 +43,9 @@ namespace MCGalaxy {
 		}
 		
 		public override void Help(Player p) {
-			Player.Message(p, "%T/MapHack");
-			Player.Message(p, "%HLets you bypass hacks restrictions on your own map");
-			Player.Message(p, "%H  (e.g. for when making a parkour map with -hax on)");
+			p.Message("%T/MapHack");
+			p.Message("%HLets you bypass hacks restrictions on your own map");
+			p.Message("%H  (e.g. for when making a parkour map with -hax on)");
 		}
 	}
 }
