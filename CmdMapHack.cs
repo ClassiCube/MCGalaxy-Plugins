3aa9a2596497e17274a8e7ccc7e2d3abe486e21b
UnknownShadow200 <unknownshadow200@gmail.com>
2021-04-26 17:53:55 +1000

    Style fixes

--- CmdMapHack.cs
+++ CmdMapHack.cs
@@ -10,41 +10,45 @@ namespace MCGalaxy {
 			get { return new[] { new CommandPerm(LevelPermission.Operator, "can bypass hack restrictions on all maps") }; }
 		}
 		
-
 		static bool hooked;
 		const string ext_allowed_key = "__MAPHACK_ALLOWED";
 		
+		void EnableHacksBypass(Player p) {
+			p.Extras[ext_allowed_key] = true;
+			p.SendMapMotd();
+			p.Message("&aYou are now bypassing hacks restrictions on this map");
+		}
+		
+		void DisableHacksBypass(Player p) {
+			p.Extras[ext_allowed_key] = false;
+			p.SendMapMotd();
+			p.Message("%HHacks bypassing reset, use %T/MapHack %Hto turn on again");
+		}
+		
 		public override void Use(Player p, string message, CommandData data) {
-
-		string[] args = message.SplitSpaces();            
-
-				if (message.Length == 0) {
-					if (!hooked) { // not thread-safe but meh
-						OnSentMapEvent.Register(HandleOnSentMap, Priority.High);
-						OnGettingMotdEvent.Register(HandleGettingMotd, Priority.High);
-						hooked = true;
-					}
-
-					if (LevelInfo.IsRealmOwner(p.name, p.level.MapName) || CheckExtraPerm(p, data, 1)) {
-						p.Extras[ext_allowed_key] = true;
-						p.SendMapMotd();
-						p.Message("&aYou are now bypassing hacks restrictions on this map");
-					} else {
-						p.Message("&cYou can only bypass hacks on your own realms.");
-					}
-				} else if (args[0].CaselessEq("stop")) {
-					p.Extras[ext_allowed_key] = false;
-					p.SendMapMotd();
-					p.Message("%HHacks bypassing reset, use %T/MapHack %Hto turn on again");
+			if (message.Length == 0) {
+				if (!hooked) { // not thread-safe but meh
+					OnSentMapEvent.Register(HandleOnSentMap, Priority.High);
+					OnGettingMotdEvent.Register(HandleGettingMotd, Priority.High);
+					hooked = true;
 				}
+
+				if (LevelInfo.IsRealmOwner(p.name, p.level.MapName) || CheckExtraPerm(p, data, 1)) {
+					EnableHacksBypass(p);
+				} else {
+					p.Message("&cYou can only bypass hacks on your own realms.");
+				}
+			} else if (message.CaselessEq("stop")) {
+				DisableHacksBypass(p);
+			} else {
+				Help(p);
+			}
 		}
 
 		void HandleOnSentMap(Player p, Level prevLevel, Level level) {
 			if (!p.Extras.GetBoolean(ext_allowed_key)) return;
 			// disable /maphack when you reload or change maps
-			p.Extras[ext_allowed_key] = false;
-			p.SendMapMotd();
-			p.Message("%HHacks bypassing reset, use %T/MapHack %Hto turn on again");
+			DisableHacksBypass(p);
 		}
 		
 		void HandleGettingMotd(Player p, ref string motd) {
--- KickNoCC.cs
+++ KickNoCC.cs
@@ -6,9 +6,8 @@ namespace MCGalaxy {
 	
 	public class PluginKickNoCC : Plugin {
 		public override string creator { get { return "aleksb385"; } } //made from KickJini.cs
-		public override string MCGalaxy_Version { get { return "1.9.0.0"; } } //this could be anything
+		public override string MCGalaxy_Version { get { return "1.9.0.0"; } }
 		public override string name { get { return "KickNoCC"; } }
-        public override bool LoadAtStartup { get { return true; } }
 
 		public override void Load(bool startup) {
 			OnPlayerConnectEvent.Register(KickNoCC, Priority.High);
@@ -21,8 +20,8 @@ namespace MCGalaxy {
 		void KickNoCC(Player p) {
 			string app = p.appName;            
 			if (app == null || !app.CaselessContains("ClassiCube ")) {
-			p.Leave("Please connect using the ClassiCube client with Enhanced mode.", true);
-			p.cancellogin = true; 
+				p.Leave("Please connect using the ClassiCube client with Enhanced mode.", true);
+				p.cancellogin = true; 
 		    }
 		}
     }
