154e764245b263a3229e5d524c0b5a612a979db5
UnknownShadow200 <unknownshadow200@gmail.com>
2023-01-18 23:53:12 +1100

    Fix CmdCopyServerMap not compiling

--- CmdCopyServerMap.cs
+++ CmdCopyServerMap.cs
@@ -1,7 +1,5 @@
-//reference System.Data.dll
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using MCGalaxy;
 using MCGalaxy.Blocks;
@@ -61,7 +59,8 @@ public sealed class CmdCopyServerMap : Command
 		// Local/Level custom blocks override global ones
 		if (File.Exists(path)) {
 			BlockDefinition[] localDefs = BlockDefinition.Load(path);
-			for (int i = 0; i < localDefs.Length; i++) {
+			for (int i = 0; i < localDefs.Length; i++) 
+			{
 				if (localDefs[i] == null || string.IsNullOrEmpty(localDefs[i].Name)) continue;
 				defs[i] = localDefs[i];
 			}
@@ -69,7 +68,8 @@ public sealed class CmdCopyServerMap : Command
 		
 		// If block was original classic/CPE block on old server, but is now a custom block on new server
 		// then make a block definition to prevent textures looking wrong (e.g. for Glass)
-		for (int i = 0; i < Block.CPE_COUNT; i++) {
+		for (int i = 0; i < Block.CPE_COUNT; i++) 
+		{
 			if (defs[i] == null && BlockDefinition.GlobalDefs[i] != null) {
 				defs[i] = DefaultSet.MakeCustomBlock((BlockID)i);
 			}
@@ -91,24 +91,27 @@ public sealed class CmdCopyServerMap : Command
 	// ===========================================================
 	static void ExportPortals(Player p, string src, string dst) {
 		List<PortalInfo> list = new List<PortalInfo>();
-		OldReadRows("Portals" + src, "EntryX,EntryY,EntryZ,ExitX,ExitY,ExitZ,ExitMap", list, ProcessPortal);
+		OldReadRows("Portals" + src, "EntryX,EntryY,EntryZ,ExitX,ExitY,ExitZ,ExitMap", 
+					row => list.Add(ParsePortal(row)));
 		if (list.Count == 0) return;
 		
 		p.Message("Creating portals table..");
-		foreach (PortalInfo i in list) {
+		foreach (PortalInfo i in list) 
+		{
 			Portal.Set(dst, i.EntryX, i.EntryY, i.EntryZ,
 			           i.ExitX, i.ExitY, i.ExitZ, i.ExitMap.Replace(src, dst));
 		}
 		p.Message("Done copying portals");
 	}
 
-	class PortalInfo {
+	class PortalInfo 
+	{
 		public ushort EntryX, EntryY, EntryZ;
 		public ushort ExitX,  ExitY,  ExitZ;
 		public string ExitMap;
 	}
 
-	static object ProcessPortal(IDataRecord record, object arg) {
+	static PortalInfo ParsePortal(ISqlRecord record) {
 		PortalInfo i = new PortalInfo();
 		i.EntryX = (ushort)record.GetInt32(0);
 		i.EntryY = (ushort)record.GetInt32(1);
@@ -119,8 +122,7 @@ public sealed class CmdCopyServerMap : Command
 		i.ExitZ  = (ushort)record.GetInt32(5);
 
 		i.ExitMap = record.GetString(6);
-		((List<PortalInfo>)arg).Add(i);
-		return arg;
+		return i;
 	}
 
 
@@ -129,62 +131,71 @@ public sealed class CmdCopyServerMap : Command
 	// ===========================================================
 	static void ExportMessages(Player p, string src, string dst) {
 		List<MessageInfo> list = new List<MessageInfo>();
-		OldReadRows("Messages" + src, "X,Y,Z,Message", list, ProcessMessage);
+		OldReadRows("Messages" + src, "X,Y,Z,Message", 
+					row => list.Add(ParseMessage(row)));
 		if (list.Count == 0) return;
 		
 		p.Message("Creating message blocks table..");
-		foreach (MessageInfo i in list) {
+		foreach (MessageInfo i in list) 
+		{
 			MessageBlock.Set(dst, i.X, i.Y, i.Z, i.Text);
 		}
 		p.Message("Done copying message blocks");
 	}
 
-	class MessageInfo {
+	class MessageInfo 
+	{
 		public ushort X, Y, Z;
 		public string Text;
 	}
 
-	static object ProcessMessage(IDataRecord record, object arg) {
+	static MessageInfo ParseMessage(ISqlRecord record) {
 		MessageInfo i = new MessageInfo();
 		i.X = (ushort)record.GetInt32(0);
 		i.Y = (ushort)record.GetInt32(1);
 		i.Z = (ushort)record.GetInt32(2);
 
 		i.Text = record.GetString(3).Replace("\\'", "\'").Cp437ToUnicode();
-		((List<MessageInfo>)arg).Add(i);
-		return arg;
+		return i;
 	}
 
 	// ===========================================================
 	// ======================== DATABASE =========================
 	// ===========================================================
-	sealed class OldSQLiteConnection : SQLiteConnection {
+	sealed class OldSQLiteConnection : SQLiteConnection 
+	{
 		protected override bool ConnectionPooling { get { return false; } }
 		protected override string DBPath { get { return Path.Combine(oldServer, "MCGalaxy.db"); } }
 	}
 	
-	static void OldReadRows(string table, string columns, object arg, ReaderCallback callback) {
+	static void OldReadRows(string table, string columns, ReaderCallback callback) {
 		using (SQLiteConnection conn = new OldSQLiteConnection()) {
 			conn.Open();
 			if (!OldTableUsed(conn, table)) return;
 			string sql = SQLiteBackend.Instance.ReadRowsSql(table, columns, "");
 
-			using (SQLiteCommand cmd = new SQLiteCommand(sql, conn)) {
-				using (IDataReader r = cmd.ExecuteReader()) {
-					while (r.Read()) callback(r, arg);
+			using (ISqlCommand cmd = conn.CreateCommand(sql)) {
+				using (ISqlReader r = cmd.ExecuteReader()) 
+				{
+					while (r.Read()) callback(r);
 				}
 			}
 		}
 	}
 	
-	static bool OldTableUsed(SQLiteConnection conn, string table) {
+	static bool OldTableUsed(ISqlConnection conn, string table) {
 		IDatabaseBackend sqliteDB = SQLiteBackend.Instance;
 		string sql = sqliteDB.ReadRowsSql("sqlite_master", "COUNT(*)", "WHERE type='table' AND name=@0");
-
-		using (SQLiteCommand cmd = new SQLiteCommand(sql, conn)) {
+		int count  = 0;
+		
+		using (ISqlCommand cmd = conn.CreateCommand(sql)) {
 			sqliteDB.FillParams(cmd, new object[] { table });
-			return cmd.ExecuteScalar() != null;
+			using (ISqlReader r = cmd.ExecuteReader()) 
+			{
+				if (r.Read()) count = r.GetInt32(0);
+			}
 		}
+		return count > 0;
 	}
 	
 	public override void Help(Player p) {
